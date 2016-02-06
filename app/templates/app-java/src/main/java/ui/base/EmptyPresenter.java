417063b5f56946b8ab0cafc64d7aeae848696708
Deividi <deividi.cavarzan@gmail.com>
2016-02-06 01:26:23 -0200

    fix activity injection when use ApplicationComponent

--- app/templates/app-java/src/main/java/ui/base/EmptyPresenter.java
+++ app/templates/app-java/src/main/java/ui/base/EmptyPresenter.java
@@ -3,7 +3,7 @@ package <%= appPackage %>.ui.base;
 import <%= appPackage %>.ui.base.BasePresenter;
 import javax.inject.Inject;
 
-public class EmptyPresenter extends BasePresenter {
+public class EmptyPresenter extends BasePresenter<PresenterView> {
 
     @Inject
     public EmptyPresenter() { }
