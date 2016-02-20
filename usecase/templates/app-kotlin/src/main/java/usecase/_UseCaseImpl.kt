633f805f7f60bfad4377c9b82cceffa5b0c8dae7
Deividi <deividi.cavarzan@gmail.com>
2016-02-20 00:44:37 -0200

    update dependencies -> new versions! added more stuff in java continue kotlin migration

--- usecase/templates/app-kotlin/src/main/java/usecase/_UseCaseImpl.kt
+++ usecase/templates/app-kotlin/src/main/java/usecase/_UseCaseImpl.kt
@@ -3,7 +3,7 @@ package <%= appPackage %>.domain.usecases.<%= useCasePackageName %>;
 import <%= appPackage %>.domain.executors.ThreadExecutor;
 import <%= appPackage %>.domain.interactors.base.BaseUseCase;
 
-public class <%= useCaseName %>UseCaseImpl(executor: ThreadExecutor) : BaseUseCase(executor), <%= useCaseName %>UseCase {
+class <%= useCaseName %>UseCaseImpl(executor: ThreadExecutor) : BaseUseCase(executor), <%= useCaseName %>UseCase {
 
     override fun invoke() {
         // TODO
