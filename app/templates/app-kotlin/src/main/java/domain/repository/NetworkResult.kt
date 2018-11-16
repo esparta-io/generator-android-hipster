809811deef432480a1ed25dfc747b213726f9b48
gmribas <gmribas1@gmail.com>
2018-11-16 19:40:25 -0200

    CoroutineDispatcher injection.

--- app/templates/app-kotlin/src/main/java/domain/repository/NetworkResult.kt
+++ app/templates/app-kotlin/src/main/java/domain/repository/NetworkResult.kt
@@ -7,4 +7,11 @@ package <%= appPackage %>.domain.repository
 sealed class NetworkResult<out T: Any> {
     data class Success<out T : Any>(val data: T) : NetworkResult<T>()
     data class Error(val exception: Throwable) : NetworkResult<Nothing>()
+
+    fun <T : Any> NetworkResult<T>.getOrThrow(throwable: Throwable? = null): T {
+        return when (this) {
+            is NetworkResult.Success -> data
+            is NetworkResult.Error -> throw throwable ?: exception
+        }
+    }
 }
